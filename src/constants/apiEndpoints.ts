b2823709f1175e7c3b3d4f8081049708e9cf87f2
JunYong <wnsdyd21@gmail.com>
2025-03-27 12:52:14 +0900

    feat: api end point 등록

---
+++ src/constants/apiEndpoints.ts
@@ -0,0 +1,15 @@
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3890';
+const PORT = process.env.PORT || 3890;
+export const API_ENDPOINTS = {
+  CRON: {
+    START: `${BASE_URL}:${PORT}/api/cron/start`,
+    STOP: `${BASE_URL}:${PORT}/api/cron/stop`,
+  },
+  MENU: {
+    WEEKLY: `${BASE_URL}:${PORT}/api/menu/weekly-menu`,
+    SEND_TO_SLACK: `${BASE_URL}:${PORT}/api/menu/send-menu-to-slack`,
+    SEND_MARKDOWN_TO_SLACK: `${BASE_URL}:${PORT}/api/menu/send-markdown-menu-to-slack`,
+  },
+} as const;
+
+export default API_ENDPOINTS;
---
+++ src/controllers/cronController.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from 'express';
+import cron from 'node-cron';
+import axios from 'axios';
+import { API_ENDPOINTS } from '../constants/apiEndpoints';
+
+interface CronTask {
+  start: () => void;
+  stop: () => void;
+}
+
+let scheduledTask: CronTask | null = null;
+
+export const startCronJob = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    if (scheduledTask === null) {
+      scheduledTask = await cron.schedule(
+        // 1분마다 실행
+        '*/1 * * * *',
+        async () => {
+          console.log('월요일 7시 실행');
+          try {
+            await axios.get(API_ENDPOINTS.MENU.SEND_TO_SLACK);
+            console.log('메뉴 전송 API 호출 성공');
+          } catch (error) {
+            console.error('메뉴 전송 API 호출 실패:', error);
+          }
+        },
+        {
+          timezone: 'Asia/Seoul',
+        },
+      );
+
+      scheduledTask!.start();
+      res.status(200).json({ message: 'cron 스케줄러가 시작되었습니다.' });
+    } else {
+      res.status(400).json({ message: '이미 cron 스케줄러가 동작 중입니다.' });
+    }
+  } catch (error) {
+    console.error('cron 스케줄러 시작 중 오류가 발생했습니다:', error);
+    res
+      .status(500)
+      .json({ message: 'cron 스케줄러 시작 중 오류가 발생했습니다.' });
+  }
+};
+
+export const stopCronJob = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    if (scheduledTask !== null) {
+      scheduledTask.stop();
+      scheduledTask = null;
+      res.status(200).json({ message: 'cron 스케줄러가 멈추었습니다.' });
+    } else {
+      res
+        .status(400)
+        .json({ message: 'cron 스케줄러가 동작하지 않고 있습니다.' });
+    }
+  } catch (error) {
+    console.error('cron 스케줄러 중지 중 오류가 발생했습니다:', error);
+    res
+      .status(500)
+      .json({ message: 'cron 스케줄러 중지 중 오류가 발생했습니다.' });
+  }
+};
